ac32c43926b9210bb72b8cae3ff0b464a81b5e4d
Si-jeong <ssonge413@gmail.com>
2021-09-16 21:29:27 +0900

    Update chapter11

--- ThisisCodingTest/Greedy/q6_무지의먹방라이브.cpp
+++ ThisisCodingTest/Greedy/q6_무지의먹방라이브.cpp
@@ -11,27 +11,27 @@ bool compare(pair<int, int> a, pair<int, int> b) {
 }
 
 int solution(vector<int> food_times, long long k) {
-    long long summary = 0; // 전체 음식을 먹는 시간
+    long long total = 0; // 전체 음식을 먹는 시간
     for (int i=0; i<food_times.size(); i++){
-        summary += food_times[i];
+        total += food_times[i];
     }
-    if (summary <= k) return -1;
+    if (total <= k) return -1;
 
     // 우선 순위 큐 (default 최대 힙 -> 최소 힙 구현)
     priority_queue<pair<int, int> > pq;
 
     for (int i=0; i<food_times.size(); i++){
-        pq.push(make_pair(-food_times[i], i+1)); // -음식섭취시간, 번호
+        pq.push({-food_times[i], i+1}); // -음식섭취시간, 번호
     }
 
-    summary = 0; // 먹기 위해 사용한 시간
+    total = 0; // 먹기 위해 사용한 시간
     long long previous = 0; // 직전에 다 먹은 시간
     long long length = food_times.size(); // 남은 음식의 개수
 
-    while(summary + ((-pq.top().first - previous) * length) <= k){
+    while(total + ((-pq.top().first - previous) * length) <= k){
         int now = -pq.top().first;
         pq.pop();
-        summary += (now-previous)*length;
+        total += (now-previous)*length;
         length -= 1;
         previous = now;
     }
@@ -41,11 +41,11 @@ int solution(vector<int> food_times, long long k) {
         int food_time = -pq.top().first;
         int num = pq.top().second;
         pq.pop();
-        result.push_back(make_pair(food_time, num));
+        result.push_back({food_time, num});
     }
 
     sort(result.begin(), result.end(), compare); //음식의 번호 기준으로 정렬
-    return result[(k-summary) % length].second;
+    return result[(k-total) % length].second;
 }
 
 int main(){
